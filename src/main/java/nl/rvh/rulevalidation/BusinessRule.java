f9c8fdc51a51f78bd3ae71a738f03e92538347d4
notedop <raoul2405@hotmail.com>
2021-06-27 13:51:09 +0200

    accept rule parameters
    All to pass on rule parameters to the business rule. Refactor
    ObjectToEvaluate to ExpectedValue which is more descriptive.

--- src/main/java/nl/rvh/rulevalidation/BusinessRule.java
+++ src/main/java/nl/rvh/rulevalidation/BusinessRule.java
@@ -2,25 +2,37 @@ package nl.rvh.rulevalidation;
 
 import nl.rvh.rulevalidation.enums.ComparisonOperator;
 
+import java.util.Map;
+
 /**
  * Abstract business rule class. Extend this class to implement custom business rule.
  * The implementing class must contain a constructor having the @JsonCreator annotation
  * and JsonProperty annotation on the constructor parameters to assure proper serialization
  * and de-serialization.
  * <p>
- * Implement the Evaluate method in the derived class. You can process the objectToEvaluate to your
+ * Implement the Evaluate method in the derived class. You can process the expectedValue to your
  * liking and then use the comparisonOperator.compare() method to do the final evaluation
  *
- * Make sure to use the @XStreamOmitField to exclude any field from serializing
+ * Use the @XStreamOmitField to exclude fields which cannot be serialized. In order to instantiate non-serializable
+ * fields after de-serialization you should override the readResolve() method.
  */
 public abstract class BusinessRule extends Rule {
 
+    protected final Map<String, String> ruleParameters;
     protected ComparisonOperator comparisonOperator;
-    protected Object objectToEvaluate;
+    protected Object expectedValue;
 
-    protected BusinessRule(ComparisonOperator comparisonOperator, Object objectToEvaluate, String name) {
+    /**
+     * Constructor for business rule
+     * @param comparisonOperator Defines which compare operator should be used by this rule
+     * @param ruleParameters Defines additional parameters which can be used by the rule
+     * @param expectedValue Defines the expected value
+     * @param name Defines the name of the rule, usually defaulted by the derived class.
+     */
+    protected BusinessRule(ComparisonOperator comparisonOperator, Map<String, String> ruleParameters, Object expectedValue, String name) {
         super(name);
-        this.objectToEvaluate = objectToEvaluate;
+        this.ruleParameters = ruleParameters;
+        this.expectedValue = expectedValue;
         this.comparisonOperator = comparisonOperator;
     }
 
@@ -32,22 +44,12 @@ public abstract class BusinessRule extends Rule {
         this.comparisonOperator = comparisonOperator;
     }
 
-    public Object getObjectToEvaluate() {
-        return objectToEvaluate;
+    public Object getExpectedValue() {
+        return expectedValue;
     }
 
-    public void setObjectToEvaluate(Object objectToEvaluate) {
-        this.objectToEvaluate = objectToEvaluate;
-    }
-
-    @Override
-    public String toString() {
-        final StringBuilder sb = new StringBuilder("BusinessRule{");
-        sb.append("comparisonOperator=").append(comparisonOperator);
-        sb.append(", objectToEvaluate=").append(objectToEvaluate);
-        sb.append(", name='").append(name).append('\'');
-        sb.append('}');
-        return sb.toString();
+    public void setExpectedValue(Object expectedValue) {
+        this.expectedValue = expectedValue;
     }
 
     @Override
@@ -58,15 +60,30 @@ public abstract class BusinessRule extends Rule {
 
         BusinessRule that = (BusinessRule) o;
 
+        if (ruleParameters != null ? !ruleParameters.equals(that.ruleParameters) : that.ruleParameters != null)
+            return false;
         if (comparisonOperator != that.comparisonOperator) return false;
-        return objectToEvaluate != null ? objectToEvaluate.equals(that.objectToEvaluate) : that.objectToEvaluate == null;
+        return expectedValue != null ? expectedValue.equals(that.expectedValue) : that.expectedValue == null;
     }
 
     @Override
     public int hashCode() {
         int result = super.hashCode();
+        result = 31 * result + (ruleParameters != null ? ruleParameters.hashCode() : 0);
         result = 31 * result + (comparisonOperator != null ? comparisonOperator.hashCode() : 0);
-        result = 31 * result + (objectToEvaluate != null ? objectToEvaluate.hashCode() : 0);
+        result = 31 * result + (expectedValue != null ? expectedValue.hashCode() : 0);
         return result;
     }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("BusinessRule{");
+        sb.append("ruleParameters=").append(ruleParameters);
+        sb.append(", comparisonOperator=").append(comparisonOperator);
+        sb.append(", expectedValue=").append(expectedValue);
+        sb.append(", name='").append(name).append('\'');
+        sb.append('}');
+        return sb.toString();
+    }
+
 }
